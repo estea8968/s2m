7661caa86a880b3ffb4448e3cd519d6fff4b975e
estea chen <estea8968@gmail.com>
2019-02-10 22:55:24 +0800

    Add files via upload

---
+++ bpi-bit-firmware/boot.py
@@ -0,0 +1,16 @@
+# This file is executed on every boot (including wake-boot from deepsleep)
+import esp
+esp.osdebug(None)
+
+#import wifi
+#wifi.start()
+
+#import webrepl
+#webrepl.start()
+
+#import webdav
+#webdav.start()
+
+#import dynamic
+#dynamic.start()
+
